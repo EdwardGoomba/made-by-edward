e2686c74dcf69d1f198683e644246fde1c88544d
EdwardGoomba <ask@edwardvdesign.com>
2019-01-10 02:56:30 -0500

    Housekeeping, added mailchimp form

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,22 +2,21 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Intro from '../components/Intro'
 
-class Home extends React.Component {
+class About extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <Intro />
+        Coming soon...
       </Layout>
     )
   }
 }
 
-export default Home
+export default About
 
 export const pageQuery = graphql`
   query {
