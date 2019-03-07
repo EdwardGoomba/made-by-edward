09f9579bd2d4b1d3e65c09c1fd6e9f17a4961676
EdwardGoomba <ask@edwardvdesign.com>
2019-03-07 18:48:51 -0500

    Update packages, update intro copy

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
     <IntroContainer>
       <h2>Hi, Im Edward Danilyuk.</h2>
       <p>Im a designer, developer, aspiring film maker and waffle enthusiast. My writing encourages sharing of ideas, knowledge and experiences among fellow makers while following a path of intentional living.</p>
-      <p>I also write Momentum, a weekly newsletter for creatives who are juggling making a living and navigating the hazards of life. If you are looking for laughs, “wtf was this guy thinking” moments or life lessons - you'll feel at home. There’s no spam and it’s totally free. Wanna be the first subscriber?
+      <p>I also write Momentum, a weekly newsletter for creatives who are juggling making a living and navigating the hazards of life. If you are looking for laughs, “wtf was this guy thinking” moments or life lessons - you'll feel at home. There’s no spam and it’s totally free. Come join us!
       </p>
       <SignupForm
         action='https://madebyedward.us7.list-manage.com/subscribe/post?u=90ad792bc930baaaa94db575c&amp;id=03d493fdc6'
